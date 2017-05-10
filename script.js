bd7609a1ac2a583d80556b3aed1e406c9b848e6a
cbartholomew69 <bartholomew_christy@yahoo.com>
2017-05-10 09:40:04 -0400

    revised  html, css and js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-var secretWord = ['javascript', 'angular', 'variables', 'syntax', 'loops' ];
+var alphabet = ['javascript', 'angular', 'variables', 'syntax', 'loops' ];
 
-function chooseWord() {
+function secretWord () {
 
-  return secretWord[Math.floor(Math.random() * words.length)];
-
+  return words[Math.floor(Math.random() * words.length)];
 }
 
-
-
+var buttons = function () {
+  myButtons = document.getElementById('buttons');
+  letters = document.createElement('ul');
+  for (var i = 0; i < alphabet.length; i++) {
+    letters.id = 'alphabet';
+    list = document.createElement('li');
+    list.id = 'letter';
+    list.innerHTML = alphabet[i];
+    check();
+    myButtons.appendChild(letters);
+    letter.appendChild(list);
+  }
+}
