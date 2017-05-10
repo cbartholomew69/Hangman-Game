54b9a04bed3303a7c3b9457f23a8bbf8f466686c
cbartholomew69 <bartholomew_christy@yahoo.com>
2017-05-10 16:46:41 -0400

    adding to all pages

--- script.js
+++ script.js
@@ -4,32 +4,48 @@ window.onload = function () {
 
 }
 
-var word="";
+var word = "";
 
 var answerArray = [];
 
 function start() {
 
   words = word[Math.floor(Math.random() * word.length)];
+
   answerArray = [];
+
   for (var i = 0; i < word.length; i++) {
+
     answerArray[i] = "_";
   }
+
   document.getElementById('answer').innerHTML = answerArray.join(" ");
+
   document.getElementById('message').innerHTML = 'Enter a letter or click quit'
 }
+
 start();
 
 function guessOne () {
+
   var guess = document.getElementById('guess').value;
+
   var showMessage = "";
+
     if (guess.length !== 1) {
+
       showMessage = "Enter a single letter!";
+
     } else {
+
   var i = 0;
+
     for (var i = 0; i < word.length; i++) {
+
       if (word[i] === guess) {
+
         answerArray[i] = guess;
+
         showMessage = "Yay! " + guess + " is part of the answer";
       }
     }
@@ -47,16 +63,32 @@ function guessOne () {
     }
       if (remainingLetters == 0){
 
-        showMessage = "You got it! BaJesus, you guessed the word";
+        showMessage = "BaJesus, you guessed the word";
 
       if (showMessage === "") {
 
         showMessage = "To dog on bad, no " + guess;
 
-document.getElementById("answer").innerHTML = answerArray.join(" ");
-document.getElementById("guess").value = "";
+ document.getElementById("answer").innerHTML = answerArray.join(" ");
+
+ document.getElementById("guess").value = "";
+  }
+
  }
+
+ document.getElementById("message").innerHTML = showMessage;
+
+
+function quit() {
+
+  document.getElementById("message").innerHTML = "The word was "+word;
+
+  for (var j = 0; j < word.length; j++) {
+
+    answerArray[i] = word [j];
+  }
 }
+  document.getElementById("answer").innerHTML = answerArray.join(" ");
 
 
 
