5ab0e644ad63c293ddf9fc7a2d3bcfb41f0e6ab2
cbartholomew69 <bartholomew_christy@yahoo.com>
2017-05-12 10:44:59 -0400

    back to showing our dashes on the screen

--- script.js
+++ script.js
@@ -2,93 +2,96 @@ window.onload = function () {
 
   var words = ['javasript', 'angular', 'syntax', 'ruby', 'jquery', 'nodes'];
 
-}
+  var answerArray = [];
 
-var word = "";
+  var randomNumber = Math.floor(Math.random() * words.length);
+  var word = words[randomNumber];
 
-var answerArray = [];
+  function start() {
+    console.log('word : ' + word);
 
-function start() {
+    // word = words[Math.floor(Math.random() * word.length)];
 
-  words = word[Math.floor(Math.random() * word.length)];
+    for (var i = 0; i < word.length; i++) {
 
-  answerArray = [];
+      answerArray[i] = "_";
 
-  for (var i = 0; i < word.length; i++) {
+    }
 
-    answerArray[i] = "_";
+    console.log('answer : ' + answerArray.join(" "));
+    document.getElementById('answer').innerHTML = answerArray.join(" ");
+    // $('#')
+    // $('#answer').innerHTML = answerArray.join(" ");
+
+    // document.getElementById('message').innerHTML = 'Enter a letter or click quit'
   }
 
-  document.getElementById('answer').innerHTML = answerArray.join(" ");
+  function guessOne () {
 
-  document.getElementById('message').innerHTML = 'Enter a letter or click quit'
-}
+    var guess = document.getElementById('guess').value;
 
-start();
-
-function guessOne () {
-
-  var guess = document.getElementById('guess').value;
-
-  var showMessage = "";
+    var showMessage = "";
 
     if (guess.length !== 1) {
 
       showMessage = "Enter a single letter!";
 
-    } else {
+    }
+    else {
 
-  var i = 0;
+      var i = 0;
 
-    for (var i = 0; i < word.length; i++) {
+      for (var i = 0; i < word.length; i++) {
 
-      if (word[i] === guess) {
+        if (word[i] === guess) {
 
-        answerArray[i] = guess;
+          answerArray[i] = guess;
 
-        showMessage = "Yay! " + guess + " is part of the answer";
+          showMessage = "Yay! " + guess + " is part of the answer";
+        }
       }
     }
   }
-}
 
   var remainingLetters = answerArray.length;
 
-    for (i = 0; i < answerArray.length; i++) {
+  for (i = 0; i < answerArray.length; i++) {
 
-      if (answerArray[i] !== '_') {
+    if (answerArray[i] !== '_') {
 
-        remainingLetters -= 1;
-      }
+      remainingLetters -= 1;
     }
-      if (remainingLetters == 0){
-
-        showMessage = "BaJesus, you guessed the word";
-
-      if (showMessage === "") {
-
-        showMessage = "To dog on bad, no " + guess;
-
- document.getElementById("answer").innerHTML = answerArray.join(" ");
-
- document.getElementById("guess").value = "";
   }
 
- }
+  if (remainingLetters == 0){
 
- document.getElementById("message").innerHTML = showMessage;
+    showMessage = "BaJesus, you guessed the word";
 
+    if (showMessage === "") {
 
-function quit() {
+      showMessage = "To dog on bad, no " + guess;
 
-  document.getElementById("message").innerHTML = "The word was "+word;
+      document.getElementById("answer").innerHTML = answerArray.join(" ");
 
-  for (var j = 0; j < word.length; j++) {
+      document.getElementById("guess").value = "";
 
-    answerArray[i] = word [j];
+      document.getElementById("message").innerHTML = showMessage;
+    }
+
   }
+
+  function quit() {
+
+    document.getElementById("message").innerHTML = "The word was "+word;
+
+    for (var j = 0; j < word.length; j++) {
+
+      answerArray[j] = word[j];
+    }
+    document.getElementById("answer").innerHTML = answerArray.join(" ");
+  }
+
+  var startButton = document.getElementById('start');
+
+  startButton.addEventListener('click', start);
 }
-  document.getElementById("answer").innerHTML = answerArray.join(" ");
-
-
-
